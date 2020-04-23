abfd273c67aaec9d6b18fda064a88eece5f7c47b
Michał Kostyk <michasio25@gmail.com>
2020-04-23 03:16:17 +0200

    quick fix

--- src/main/java/com/soundspark/soundsparkbackend/SoundsparkBackendApplication.java
+++ src/main/java/com/soundspark/soundsparkbackend/SoundsparkBackendApplication.java
@@ -1,9 +1,13 @@
 package com.soundspark.soundsparkbackend;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+//TODO::ogarnac Data Source error
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class SoundsparkBackendApplication {
 
 	public static void main(String[] args) {
