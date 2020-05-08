20fba484ab3ae9316cbbbc995c83b97f69b3cc6c
Michał Kostyk <michasio25@gmail.com>
2020-05-08 19:34:24 +0200

    integrate handleTranscription with ASR service

--- src/main/java/com/soundspark/soundsparkbackend/rest/controller/TranscriptionController.java
+++ src/main/java/com/soundspark/soundsparkbackend/rest/controller/TranscriptionController.java
@@ -1,34 +1,41 @@
 package com.soundspark.soundsparkbackend.rest.controller;
 
 import com.soundspark.soundsparkbackend.model.TranscriptionResponse;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.*;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.sound.sampled.AudioFileFormat;
-import javax.sound.sampled.AudioSystem;
 import java.io.File;
+import java.io.IOException;
 
 @CrossOrigin(origins = {"http://localhost:4200", "https://soundspark.herokuapp.com"})
 @RestController
 public class TranscriptionController {
+    String transcriptionURL = "https://soundspark-model.herokuapp.com/asr";
 
     @PostMapping("/transcription")
-    public ResponseEntity<TranscriptionResponse> handleTranscription(@RequestParam("file") MultipartFile file) {
-        try {
-            File convFile = new File(System.getProperty("java.io.tmpdir") + "/" + file.getOriginalFilename());
-            file.transferTo(convFile);
-            AudioFileFormat.Type audioFormat = AudioSystem.getAudioFileFormat(convFile).getType();
-            if (!new AudioFileFormat.Type("WAVE", "wav").equals(audioFormat)) {
-                throw new Exception("Wrong audio format");
-            }
-            String mockedTranscription = "Lorem ipsum dolor sit amet, consectetur adipiscing elit." +
-                    " In ac aliquet diam. Vestibulum ante ipsum primis in faucibus orci luctus.";
-            return ResponseEntity.status(HttpStatus.OK).body(new TranscriptionResponse(mockedTranscription));
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE).build();
-        }
+    public ResponseEntity<TranscriptionResponse> handleTranscription(@RequestParam("file") MultipartFile requestFile) throws IOException {
+
+        File file = new File(System.getProperty("java.io.tmpdir") + "/" + requestFile.getOriginalFilename());
+        requestFile.transferTo(file);
+
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        body.add("file", new FileSystemResource(file.getPath()));
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body);
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<String> response = restTemplate
+                .postForEntity(transcriptionURL, requestEntity, String.class);
+        return ResponseEntity.status(HttpStatus.OK).body(new TranscriptionResponse(response.getBody()));
     }
 }
 
