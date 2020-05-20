5d82e6118af1fbc787897074d5455bf491a516cf
Michał Kostyk <michasio25@gmail.com>
2020-05-20 02:48:37 +0200

    refactor/cleanup

--- src/main/java/com/soundspark/soundsparkbackend/SoundsparkBackendApplication.java
+++ src/main/java/com/soundspark/soundsparkbackend/SoundsparkBackendApplication.java
@@ -4,14 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
-//TODO::ogarnac Data Source error
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class SoundsparkBackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SoundsparkBackendApplication.class, args);
 	}
-
 }
--- src/main/java/com/soundspark/soundsparkbackend/rest/controller/TranscriptionController.java
+++ src/main/java/com/soundspark/soundsparkbackend/rest/controller/TranscriptionController.java
@@ -1,41 +1,39 @@
 package com.soundspark.soundsparkbackend.rest.controller;
 
 import com.soundspark.soundsparkbackend.model.TranscriptionResponse;
-import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.*;
 import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
+
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.IOException;
-
 @CrossOrigin(origins = {"http://localhost:4200", "https://soundspark.herokuapp.com"})
 @RestController
 public class TranscriptionController {
-    String transcriptionURL = "https://soundspark-model.herokuapp.com/asr";
+    private final String TRANSCRIPTION_API_URL = "https://soundspark-model.herokuapp.com/asr";
 
     @PostMapping("/transcription")
-    public ResponseEntity<TranscriptionResponse> handleTranscription(@RequestParam("file") MultipartFile requestFile) throws IOException {
+    public ResponseEntity<TranscriptionResponse> handleTranscription(@RequestParam("file") MultipartFile file) {
+        ResponseEntity<String> response = requestTranscription(file.getResource());
+        if (response.getStatusCode().is2xxSuccessful()) {
+            return ResponseEntity.ok(new TranscriptionResponse(response.getBody()));
+        } else {
+            return ResponseEntity.status(response.getStatusCode()).build();
+        }
+    }
 
-        File file = new File(System.getProperty("java.io.tmpdir") + "/" + requestFile.getOriginalFilename());
-        requestFile.transferTo(file);
-
-        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
-        body.add("file", new FileSystemResource(file.getPath()));
-
+    private ResponseEntity<String> requestTranscription(Resource file) {
+        LinkedMultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        body.add("file", file);
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.MULTIPART_FORM_DATA);
-
-        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body);
-
+        HttpEntity<LinkedMultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body);
         RestTemplate restTemplate = new RestTemplate();
-
         ResponseEntity<String> response = restTemplate
-                .postForEntity(transcriptionURL, requestEntity, String.class);
-        return ResponseEntity.status(HttpStatus.OK).body(new TranscriptionResponse(response.getBody()));
+                .postForEntity(TRANSCRIPTION_API_URL, requestEntity, String.class);
+        return response;
     }
 }
 
