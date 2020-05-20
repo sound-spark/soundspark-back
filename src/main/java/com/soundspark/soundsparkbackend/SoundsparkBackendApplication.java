888c74a828fcb5efe972f38d2a8500218f45e0d3
Michał Kostyk <michasio25@gmail.com>
2020-05-20 02:56:27 +0200

    quick fix

--- src/main/java/com/soundspark/soundsparkbackend/SoundsparkBackendApplication.java
+++ src/main/java/com/soundspark/soundsparkbackend/SoundsparkBackendApplication.java
@@ -1,10 +1,7 @@
 package com.soundspark.soundsparkbackend;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 public class SoundsparkBackendApplication {
